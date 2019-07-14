1948a5e4bcf512cd8b48abe836fa2c7912724e55
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2019-07-14 04:20:40 -0300

    flyway config only one database

---
+++ src/main/resources/db/migration/V1_20190714__inicial.sql
@@ -0,0 +1,12 @@
+create table user_details
+(
+	user_id int auto_increment
+		primary key,
+	username varchar(255) null,
+	first_name varchar(50) null,
+	last_name varchar(50) null,
+	gender varchar(10) null,
+	password varchar(50) null,
+	status tinyint(10) null
+)
+
