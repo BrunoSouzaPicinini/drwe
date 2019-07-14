b6332176c25c8e72b13e1e29ecc1615fc2ed31f0
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2019-07-13 22:38:38 -0300

    initial

---
+++ src/main/java/br/com/bspicinini/drwe/DrweApplication.java
@@ -0,0 +1,13 @@
+package br.com.bspicinini.drwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrweApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrweApplication.class, args);
+    }
+
+}
